acf8cfcc9ecee28a1b67ddcaaf6692d47c8ba2d5
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-04-24 16:03:40 +0700

    design fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Search from './components/Search'
+import logo from './images/logo.png'
 
 function App() {
 
   return (
     <div className='App'>
       <div className='container'>
-        <span className='rainbow-text body'>MARVELOUS COMICS</span>
+        <span className='rainbow-text body'>Created by Nguyen Tuan Nguyen An</span>
+        <a href="/">
+        <img src={logo} alt="logo" className='logo' />
+        </a>
       </div>
-      <p className='dev-name'>Created by Nguyen Tuan Nguyen An</p>
+      
+      <p className='dev-name'></p>
       <Search/>
     </div>
   )
