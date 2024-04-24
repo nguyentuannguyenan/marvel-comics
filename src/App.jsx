d3a87982a673f679436fbf1dba8d89e1f060061b
nguyentuannguyenan <nguyenan29072001@gmail.com>
2024-04-24 15:13:01 +0700

    design fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
 
   return (
     <div className='App'>
+      <div className='container'>
+        <span className='rainbow-text body'>MARVELOUS COMICS</span>
+      </div>
+      <p className='dev-name'>Created by Nguyen Tuan Nguyen An</p>
       <Search/>
     </div>
   )
